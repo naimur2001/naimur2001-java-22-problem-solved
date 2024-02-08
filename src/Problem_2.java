901cb54702b3e963d18ed36a7a82bdc9dc4a9351
naimur <naimurrahman18thapril@gmail.com>
2024-02-09 00:23:15 +0600

    problem 2 solved

--- src/Problem_2.java
+++ src/Problem_2.java
@@ -1,4 +1,12 @@
 //2. Write a Java program to print all negative elements in an array.
 public class Problem_2 {
+    public static void main(String[] args) {
+        int[] number_array={2,4,-5,2,-1,7,-9,3,-6,8,12,-15};
 
+        for (int i = 0; i < number_array.length; i++) {
+            if (number_array[i]<0) {
+                System.out.println(number_array[i]);
+            }
+        }
+    }
 }
