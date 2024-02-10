f84ef13334feed500f58fc47e62cb94a4d1040a0
naimur <naimurrahman18thapril@gmail.com>
2024-02-10 14:20:37 +0600

    problem 15 solved

--- src/Problem_15.java
+++ src/Problem_15.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //15. Write a Java program to merge two arrays into a third array.
 public class Problem_15 {
     public static void main(String[] args) {
         int[]  array_1={6,9,4,1,5,2};
         int[]  array_2={20,7,8,3};
-        ArrayList<Integer>array_3=new ArrayList<Integer>();
+//        ArrayList<Integer>array_3=new ArrayList<Integer>();
+        int[] array_3=new int[array_1.length+ array_2.length];
         for (int i = 0; i < array_1.length; i++) {
-            array_3.add(array_1[i]);
+            array_3[i]=array_1[i];
         }
         for (int i = 0; i < array_2.length; i++) {
-            array_3.add(array_2[i]);
+            array_3[array_1.length+i]=array_2[i];
         }
-        System.out.println(array_3);
+        System.out.println(Arrays.toString(array_3));
     }
 }
---
+++ src/Problem_16.java
@@ -0,0 +1,6 @@
+//16. Write a Java program to find the reverse of an array.
+public class Problem_16 {
+    public static void main(String[] args) {
+
+    }
+}
