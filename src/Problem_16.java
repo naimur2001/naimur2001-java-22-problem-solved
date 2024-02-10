2a57862383021d3a46cf60ef8c53e3f692156c83
naimur <naimurrahman18thapril@gmail.com>
2024-02-10 14:30:12 +0600

    problem 16 solved

--- src/Problem_16.java
+++ src/Problem_16.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 //16. Write a Java program to find the reverse of an array.
 public class Problem_16 {
     public static void main(String[] args) {
-
+        int[]  array={6,9,4,1,5,2};
+        int[] reverse_array=new int[array.length];
+        for (int i = 0; i <array.length; i++) {
+       reverse_array[i]=array[(array.length-1)-i];
+        }
+        System.out.println(Arrays.toString(reverse_array));
     }
 }
